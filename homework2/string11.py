4e5a86089e9061caaabe22f3d14c9bd89865df78
odilbekmarimov <o.marimov@newuu.uz>
2024-10-27 21:36:04 +0500

    Update string11.py

--- homework2/string11.py
+++ homework2/string11.py
@@ -2,3 +2,5 @@ a = input ("input the text: ")
 test = input ("input the test character: ")
 
 print(f"position of first occurance of '{test}' is:", a.index(test))
+#or
+print(f"position of first occurance of '{test}' is:", a.find(test))
